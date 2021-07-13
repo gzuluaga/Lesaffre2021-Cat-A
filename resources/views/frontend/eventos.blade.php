680807664e22a6a2ad8df3250ac7160203e6a39f
gzuluaga <thekurai@gmail.com>
2021-07-13 10:41:35 -0500

    ajustes eventos

--- resources/views/frontend/eventos.blade.php
+++ resources/views/frontend/eventos.blade.php
@@ -16,17 +16,29 @@
                         data-bg-image="assets/img/slider/splash.png">
                         
                         <div class="container">
-                            <h4>Eventos</h4>
+                            <h3>Eventos Lesaffre 2021</h3>
+                            <br>
+                            <br>
                             @foreach ($eventos as $evento)
-                            <div class="row">
-                                <div class="col-sm-12 col-md-12 col-xs-12 col-lg-12" id="bloque">
-                                    <b>Fecha del Evento:</b> {{ $evento->start }} <br>
-                                    <b>Fecha de Cierre:</b> {{ $evento->end }} <br>
-                                    <b>Evento:</b> {{ $evento->title }} <br>
-                                    <b>Enlace de conexión:</b> <a href="{{ $evento->enlace }}" target="_blank" style="color:#FFFFFF"> {{ $evento->enlace }}</a>
+                            <div class="row" style="padding-right: 10%;">
+                                <div class="col-sm-12 col-md-12 col-xs-12 col-lg-12">
+                                    <div class="row">
+                                        <div class="col-sm-2 col-md-2 col-xs-2 col-lg-2">
+                                            <center style="margin-top: 18%">
+                                                <i class="far fa-calendar-check fa-8x"></i>
+                                            </center>
+                                        </div>
+                                        <div class="col-sm-10 col-md-10 col-xs-10 col-lg-10 " id="bloque">
+                                            <b>Fecha del Evento:</b> {{ $evento->start }} <br>
+                                            <b>Fecha de Cierre:</b> {{ $evento->end }} <br>
+                                            <b>Evento:</b> {{ $evento->title }} <br>
+                                            <b>Enlace de conexión:</b> <a href="{{ $evento->enlace }}" target="_blank" style="color:#FFFFFF"> {{ $evento->enlace }}</a> <br>
+                                            <b>Notas del evento:</b> {{ $evento->notas }} <br>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div style="margin-top: 2%;"></div>
+                            <div style="margin-top: 5%;"></div>
                             @endforeach
                         </div>
                         
